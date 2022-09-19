bb0fe5a1d90116cabab0fbabb257448f54e0719a
Alberto Garcia <alberto.gdepablos@bosonit.com>
2022-09-19 09:01:10 +0200

    Initial commit

---
+++ src/main/java/com/example/ej5/logging/MainController.java
@@ -0,0 +1,22 @@
+package com.example.ej5.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Slf4j
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        log.trace("Mensaje a nivel de TRACE");
+        log.debug("Mensaje a nivel de DEBUG");
+        log.info("Mensaje a nivel de INFO");
+        log.warn("Mensaje a nivel de WARNING");
+        log.error("Mensaje a nivel de ERROR");
+
+        return "Hola! Mira los logs para ver los resultados";
+    }
+}
